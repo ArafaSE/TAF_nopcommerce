a571ebca2499ce804fab71158ee41f2947470dc7
ArafaSE <mohamed.arafa.swt@gmail.com>
2020-07-04 16:10:25 +0200

    Add Chrome driver headless option

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,6 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
@@ -20,30 +21,44 @@ public class TestBase extends AbstractTestNGCucumberTests {
 
     @BeforeSuite
     @Parameters({"browser"})
-    public void startDriver(@Optional("headlessPhantomJs") String browserName){
-        if (browserName.equalsIgnoreCase("chrome")){
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+    public void startDriver(@Optional("chrome-headless") String browserName) {
+        if (browserName.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver",
+                    System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
             System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
             driver = new ChromeDriver();
-        } else if (browserName.equalsIgnoreCase("firefox")){
-            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\drivers\\geckodriver.exe");
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver",
+                    System.getProperty("user.dir") + "\\drivers\\geckodriver.exe");
             driver = new FirefoxDriver();
         }
         // headless browser testing with phantomJs
-        else if(browserName.equalsIgnoreCase("headlessPhantomJs")){
+        else if (browserName.equalsIgnoreCase("headlessPhantomJs")) {
             DesiredCapabilities caps = new DesiredCapabilities();
             caps.setJavascriptEnabled(true);
             caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
                     System.getProperty("user.dir") + "\\drivers\\phantomjs.exe");
-            String[] phantomJsArgs = {"--web-security=no","--ignore-ssl-errors=yes"};
+            String[] phantomJsArgs = {"--web-security=no", "--ignore-ssl-errors=yes"};
             caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, phantomJsArgs);
 
             driver = new PhantomJSDriver(caps);
+        }
+        // headless browser testing with Chrome headless option
+        else if (browserName.equalsIgnoreCase("chrome-headless")) {
+            System.setProperty("webdriver.chrome.driver",
+                    System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+            System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+
+            ChromeOptions option = new ChromeOptions();
+            option.addArguments("--headless");
+            option.addArguments("--window-size=1920,1080");
+            driver = new ChromeDriver(option);
         }
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.navigate().to(homeUrl);
+
     }
 
     @AfterSuite
@@ -54,8 +69,8 @@ public class TestBase extends AbstractTestNGCucumberTests {
 
     // take screenshot when test case fail and add it in the screenshots folder
     @AfterMethod
-    public void screenshotOnFailure(ITestResult result){
-        if (result.getStatus() == ITestResult.FAILURE){
+    public void screenshotOnFailure(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
             System.out.println("Failed! - Taking screenshots..");
             Helper.captureScreenshot(driver, result.getName());
         }
